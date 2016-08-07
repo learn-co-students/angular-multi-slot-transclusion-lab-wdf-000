0be395aaeae740dc9021918e2c4f90889fd169fd
lastromanticx <giladbarkan@live.com>
2016-08-06 22:42:10 -0400

    seems to work in browser

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,10 +1,16 @@
 function UserProfile() {
 	return {
-		transclude: true,
+		transclude: {
+      name: 'h4',
+      position: 'h6',
+      description: 'p'
+    },
 		template: [
 			'<div>',
 				'<h3>User Profile</h3>',
-				'<div ng-transclude></div>',
+				'<div ng-transclude="name"></div>',
+        '<div ng-transclude="position"></div>',
+        '<div ng-transclude="description"></div>',
 			'</div>'
 		].join('')
 	}
