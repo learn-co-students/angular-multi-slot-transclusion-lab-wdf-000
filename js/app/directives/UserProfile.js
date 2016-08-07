a5d43cd78658a5062e53845e4d3efdefc79c5688
lastromanticx <giladbarkan@live.com>
2016-08-07 07:02:17 -0400

    passed test

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -8,8 +8,11 @@ function UserProfile() {
 		template: [
 			'<div>',
 				'<h3>User Profile</h3>',
+        '<h5>Name</h5>',
 				'<div ng-transclude="name"></div>',
+        '<h5>Position</h5>',
         '<div ng-transclude="position"></div>',
+        '<h5>Description</h5>',
         '<div ng-transclude="description"></div>',
 			'</div>'
 		].join('')
